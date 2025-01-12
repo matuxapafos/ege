1367b7710660efde1bf01ce4b4638a5a089f78c0
Matvey Dronov <dronov.matwey@gmail.com>
2025-01-13 00:47:48 +0300

    16

---
+++ 16.py
@@ -0,0 +1,39 @@
+# from functools import *
+
+# @lru_cache(None)
+# def f(n):
+#     sumi = 0
+#     for i in str(n):
+#         sumi+= int(i)
+#     if n < 3:
+#         return 1
+#     if n > 2 and sumi % 2 == 0: return f(n-1) - f(n-2)
+#     if n > 2 and sumi % 2 != 0: return f(n-1) + f(n//2)
+
+# for n in range(1,101): f(n)
+
+# print(f(100))
+        
+            
+from functools import *
+
+@lru_cache(None)
+def f(n):
+    if n < 3:
+        return 1
+    if n > 2 and n % 2 == 0:
+        return g(n) + f(n-1)
+    if n > 2 and n % 2 != 0:
+        return f(n-2) - 2*g(n+1)
+    
+def g(n):
+    if n < 3:
+        return 1
+    if n > 2 and n % 2 == 0:
+        return f(n-3) + f(n-2)
+    if n > 2 and n % 2 != 0:
+        return f(n+1) - g(n-1)
+    
+for n in range( 1,200): g(n)
+
+print(g(120)) 
