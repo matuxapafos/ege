83e5ace8fd88046796a46059e2c3e5342c36f5ff
Matvey Dronov <dronov.matwey@gmail.com>
2025-05-06 21:03:27 +0300

    +

---
+++ probnik_april_1/2.py
@@ -0,0 +1,13 @@
+from itertools import *
+
+def f(x,y,z):
+    return (not(x==(y <= z)))
+
+t = (
+    (0,0,1,1),
+    (0,1,1,0)
+)
+if len(t) == len(set(t)):
+    for p in permutations('xyz',r = 3):
+        if all(f(**dict(zip(p,l))) == l[-1] for l in t):
+            print(*p)
