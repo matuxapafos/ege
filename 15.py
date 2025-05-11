ea371347cd3cb17c227a991b9f13ccb855164522
Matvey Dronov <dronov.matwey@gmail.com>
2025-05-11 13:33:09 +0300

    +

--- 15.py
+++ 15.py
@@ -6,9 +6,34 @@
 #         print(a)
 #         break
 
-def deli(x,y):
-    return x % y == 0
+# def deli(x,y):
+#     return x % y == 0
+#
+# for a in range(1,10000):
+#     if all((not(deli(x,7) and deli(x,13)) <= (x > a -40)) for x in range(1,10000)):
+#         print(a)
 
-for a in range(1,10000):
-    if all((not(deli(x,7) and deli(x,13)) <= (x > a -40)) for x in range(1,10000)):
-        print(a)
+# def deli(n,m):
+#     return n%m==0
+# def sumb(s,d):
+#     return s + d >0
+#
+# def f(x,a):
+#     return (x+a>=160) or (deli(x,7) <= (not(sumb(x,-17))))
+#
+# print(min(a for a in range(1,200) if all(f(x,a) for x in range(1,400))))
+
+# def f(x,a):
+#     return (x&29 == 0 ) or ((x&11 == 0) <= (not(x&a==0)))
+#
+# print(min(a for a in range(0,200) if all(f(x,a) for x in range(15,31))))
+
+# def f(x,y,a):
+#     return (x + 2*y != 58) or ((a - x>0 ) == (a + y>0))
+#
+# print(min(a for a in range(1,200) if all(f(x,y,a) for x in range(1,400) for y in range(1,400))))
+
+def f(x,y,a):
+    return (x+y <= 32) or ( y <= x+4) or (y>=a)
+
+print(max(a for a in range(0,500) if all(f(x,y,a) for x in range(0,600) for y in range(0,60))))
