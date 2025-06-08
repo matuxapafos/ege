bedac08acbf0a8897840778f7bd9db0cfd44f263
matvejdronov <dronov.matwey@gmail.com>
2025-06-08 13:51:15 +0300

    +

--- 15.py
+++ 15.py
@@ -1,17 +1,18 @@
 # def f(x):
 #     return (x & 57 == 0) or ((x & 23 == 0) <= (not(x & a == 0)))
 
+
 # for a in range(1,1000):
 #     if all(f(x) for x in range(1,100000)):
 #         print(a)
 #         break
 
-def deli(x,y):
-    return x % y == 0
-
-for a in range(1,10000):
-    if all((not(deli(x,7) and deli(x,13)) <= (x > a -40)) for x in range(1,10000)):
-        print(a)
+# def deli(x,y):
+#     return x % y == 0
+#
+# for a in range(1,10000):
+#     if all((not(deli(x,7) and deli(x,13)) <= (x > a -40)) for x in range(1,10000)):
+#         print(a)
 
 # def deli(n,m):
 #     return n%m==0
@@ -38,7 +39,102 @@ for a in range(1,10000):
 #
 # print(max(a for a in range(0,500) if all(f(x,y,a) for x in range(0,600) for y in range(0,60))))
 
-def f(x,y,a):
-    return (2*y + 3*x != 135) or (y > a) or (x > a)
+# def f(x,y,a):
+#     return (2*y + 3*x != 135) or (y > a) or (x > a)
+#
+# print(max(a for a in range(0,200) if all(f(x,y,a) for x in range(0,400) for y in range(0,400))))
 
-print(max(a for a in range(0,200) if all(f(x,y,a) for x in range(0,400) for y in range(0,400))))
+
+# def f(x):
+#     p = 17 <= x <= 65
+#     q = 29<= x <= 75
+#     a = a1 <= x <= a2
+#     return
+#
+# r = []
+# d = [y for x in (17,29,58,80) for y in (x, x-0.1,x+0.1)]
+#
+# for a1 in d:
+#     for a2 in d:
+#         if all(f(x) == 1 for x in d) and a2 >= a1:
+#             r += [a2-a1]
+# print(round(min(r)))
+
+# def d(n,m):
+#     return n % m == 0
+#
+# for a in range(1,200):
+#     if all((d(x,3) <= (not (d(x,2)))) or (x-a >= 4) for x in range(1,400)):
+#         print(a)
+
+# for x in range(-500,100):
+#      p = x in [2,4,6,8,10,12,14,16,18,20]
+#      q = x in [5,10,15,20,25,30,25,40,45,50]
+#      a = 1
+#      f_usl = 1
+#      f = (a <= p) and (q <= (not a))
+#      if f == f_usl:
+#          print(x)
+
+
+# def f(x):
+#     p = 13<= x <= 19
+#     q = 17 <= x <= 23
+#     a = a1 <= x <= a2
+#     return (not(not(p)<= q) <= q) <= (a <= ((not q) <= p))
+# r = []
+# d = [y for x in (13,17,19,23) for y in (x,x-0.1,x+0.1)]
+# for a1 in d:
+#     for a2 in d:
+#         if all(f(x) == 1 for x in d) and a2 >= a1:
+#             r += [a2-a1]
+# print(round(max(r)))
+
+
+# def f(x):
+#     p = 12 <= x <= 28
+#     q = 15 <= x <= 30
+#     a = a1 <= x <= a2
+#     return ((p) <= (a)) and (((not(q)) or (a)))
+#
+# r = []
+# d = [y for x in (12,15,28,30) for y in (x,x-0.1,x+0.1)]
+#
+# for a1 in d:
+#     for a2 in d:
+#         if all(f(x) == 1 for x in d) and a2 >= a1:
+#             r += [a2-a1]
+# print(round(min(r)))
+
+# def d(n,m):
+#     return n % m == 0
+#
+# for a in range(1,200):
+#     if all((d(x,2) <= (not(d(x,3)))) or ((x + a >= 100)) for x in range(1,400)):
+#         print(a)
+
+
+# def f(x,y,a):
+#     return (x + 2 * y != 58) or ((a - x>0) == (a + y > 0))
+#
+# print(min(a for a in range(1,200) if all(f(x,y,a) for x in range(1,400) for y in range(1,400))))
+
+
+# def p(n,m):
+#     if n - m >= 0:
+#         return 1
+#     else:
+#         return 0
+# for a in range(0,200):
+#     if all((not (p(x +y, 73 ))) or (not ( p(37,x-y) )) or p(y,a) for x in range(0,400) for y in range(0,400)):
+#         print(a)
+
+def d(n,m):
+     return n % m == 0
+
+def sumb(s,d):
+    return s + d > 0
+
+for a in range(1,200):
+    if all( (x + a >= 160) or d(x,7) <= (not(sumb(x,-17))) for x in range(1,400)):
+        print(a)
--- 16.py
+++ 16.py
@@ -38,20 +38,81 @@
 #
 # print(g(120))
 
-from functools import *
-import sys
-sys.setrecursionlimit(5000)
-c = 0
-@lru_cache(None)
-def f(n):
-    if n == 0:
-        return 0
-    if n > 0 and n % 2 == 0:
-        return f(n//2)
-    if  n % 2 != 0:
-        return 1+f(n-1)
+# from functools import *
+# import sys
+# sys.setrecursionlimit(5000)
+# c = 0
+# @lru_cache(None)
+# def f(n):
+#     if n == 0:
+#         return 0
+#     if n > 0 and n % 2 == 0:
+#         return f(n//2)
+#     if  n % 2 != 0:
+#         return 1+f(n-1)
+#
+# for n in range(1,501):
+#     if f(n) == 8:
+#         c+= 1
+# print(c)
+# from functools import lru_cache
+#
+# @lru_cache(None)
+# def f(n):
+#     if n == 1: return 1
+#     if n % 2 == 0: return n + f(n-1)
+#     if n % 2 != 0 and n > 1: return 2 * f(n-2)
+#
+# for n in range(1,2000): f(n)
+#
+# print(f(26))
 
-for n in range(1,501):
-    if f(n) == 8:
-        c+= 1
-print(c)
+# from functools import lru_cache
+#
+# @lru_cache(None)
+# def f(n):
+#     if n == 0: return 1
+#     if n == 1: return 3
+#     if n == 2: return  2
+#     if n > 2: return f(n-1) * f(n-3)
+#
+# for n in range(2,7): f(n)
+#
+# print(f(7))
+
+# from functools import lru_cache
+#
+# @lru_cache(None)
+# def f(n):
+#     if n <= 3:return 1
+#     if n>3: return (n+3) * f(n-2)
+#
+# for n in range(3,3000): f(n)
+#
+# print(f(2028)/f(2024))
+
+
+# from functools import lru_cache
+#
+# @lru_cache(None)
+# def f(n):
+#     if n < 3: return 2**1024
+#     if n >2: return 2*n + 3 + f(n-2)
+#
+# for n in range(1,3000):
+#     f(n)
+#
+# print(f(4048) - f(16))
+
+# from functools import lru_cache
+#
+# @lru_cache(None)
+# def f(n):
+#     if n <3: return 2
+#     if n > 2 and n % 2 ==0: return 2*f(n-2)-f(n-1)+2
+#     if n>2 and n % 2 !=0: return 2*f(n-1) + f(n-2) - 2
+#
+# for n in range(1, 200): f(n)
+#
+# print(f(170))
+
